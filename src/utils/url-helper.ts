ac2aa9cd82525f000dcd4a1d6102844ceb7be17e
Shobon03 <66133043+Shobon03@users.noreply.github.com>
2025-11-23 11:51:16 -0300

    Adjust environment variable name

--- src/utils/url-helper.ts
+++ src/utils/url-helper.ts
@@ -1,7 +1,7 @@
-export const base = import.meta.env.BASE_URL;
+export const base = import.meta.env.BASE_PATH;
 export const link = (path: string) => `${base}${path.replace(/^\//, '')}`;
 export const joinPath = (path: string) => {
-  const base = import.meta.env.BASE_URL;
+  const base = import.meta.env.BASE_PATH;
   // Remove a barra inicial do path se ela existir para evitar "//"
   const cleanPath = path.startsWith('/') ? path.slice(1) : path;
   return `${base}${cleanPath}`;
