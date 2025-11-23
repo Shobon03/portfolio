1ea896eb253afebebd9bfa48580cc49811b3701d
Shobon03 <66133043+Shobon03@users.noreply.github.com>
2025-11-23 11:39:26 -0300

    Add .env and adjust deployment to use the correct Github Pages url

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
 // @ts-check
 
-import mdx from "@astrojs/mdx";
-import react from "@astrojs/react";
-import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "astro/config";
+import mdx from '@astrojs/mdx';
+import react from '@astrojs/react';
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	vite: {
-		plugins: [tailwindcss()],
-	},
-
-	integrations: [react(), mdx()],
+  site: process.env.SITE ?? undefined,
+  base: process.env.BASE_PATH ?? '/',
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  integrations: [react(), mdx()],
 });
