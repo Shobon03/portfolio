e7b35f1168c9736f18ac25e1a0537d88d5f819c8
Shobon03 <66133043+Shobon03@users.noreply.github.com>
2026-04-22 15:03:30 -0300

    feat: add homepage, change accent colors, add prettier, LICENSE and GitHub Actions

---
+++ src/util/urls.ts
@@ -0,0 +1,5 @@
+export const urls = {
+	github: "https://github.com/shobon03",
+	avatar: "https://avatars.githubusercontent.com/u/66133043?v=4",
+	linkedin: "https://www.linkedin.com/in/matheus-teixeira-020584229/",
+};
