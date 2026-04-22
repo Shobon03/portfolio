018c40f279e225d28cbf9853ca3ec2472575cdde
Shobon03 <66133043+Shobon03@users.noreply.github.com>
2026-04-22 15:09:26 -0300

    fix: adjust workflow pipelines

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+	site: "https://shobon03.github.io",
+	base: "/portfolio",
 	integrations: [mdx()],
 	vite: {
 		plugins: [tailwindcss()],
