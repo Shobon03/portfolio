b2d1dd533098e2ce6cdc5c9c02409ce992c132df
Shobon03 <66133043+Shobon03@users.noreply.github.com>
2026-04-22 17:50:48 -0300

    feat: add About page

--- src/utils/urls.ts
+++ src/utils/urls.ts
@@ -1,5 +1,5 @@
 export const urls = {
 	github: "https://github.com/shobon03",
-	avatar: "https://avatars.githubusercontent.com/u/66133043?v=4",
+	avatar: "https://github.com/Shobon03.png",
 	linkedin: "https://www.linkedin.com/in/matheus-teixeira-020584229/",
 };
